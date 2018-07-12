73e665d1ee8594c62ed8fe0616b9e4facf653f64
Sthuthi <sthuthisadananda@gmail.com>
2018-07-12 21:23:58 +0530

    with database

---
+++ app/src/main/java/ddsmartlabs/noteit/Data.kt
@@ -0,0 +1,4 @@
+package ddsmartlabs.noteit
+
+class Data(val Id:Any,val Usn:String,val Email:String){
+}
--- app/src/main/java/ddsmartlabs/noteit/MainActivity.kt
+++ app/src/main/java/ddsmartlabs/noteit/MainActivity.kt
@@ -3,16 +3,50 @@ package ddsmartlabs.noteit
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var usn: EditText
+    private lateinit var email: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setContentView(R.layout.activity_main)
         val b = findViewById<Button>(R.id.tap)
-        b.setOnClickListener { Toast.makeText(baseContext, "First Login", Toast.LENGTH_SHORT).show() }
+        usn = findViewById(R.id.usn)
+        email = findViewById(R.id.email)
+        b.setOnClickListener {
+            Toast.makeText(baseContext, "First Login", Toast.LENGTH_SHORT).show()
+            saveData()
+        }
     }
+
+    private fun saveData() {
+        val Usn=usn.text.toString();
+        if(Usn.isEmpty())
+        {
+            usn.error="Please enter USN"
+            return
+        }
+        val Email=email.text.toString();
+        if(Email.isEmpty()||!(Email.contains('@')))
+        {
+
+            email.error="Please enter valid EmailId"
+            return
+        }
+
+        val ref= FirebaseDatabase.getInstance().getReference("Students")
+        val Id=ref.push().key.toString()
+        val Student=Data(Id,Usn,Email)
+        ref.child(Id).setValue(Student).addOnCompleteListener {
+            Toast.makeText(baseContext,"Data Saved",Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
+
